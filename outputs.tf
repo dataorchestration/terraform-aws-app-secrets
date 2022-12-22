e31306be0bb6b4138bd65cea3b979f02c4e36eb7
navdeep710 <navdeep710@gmail.com>
2022-12-22 11:26:37 +0530

    remove sensitive flag

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "all" {
   description = "Map of names and arns of created secrets"
-  value = [
-    for name in keys(local.secrets) : {
-      name = upper(replace(name, "-", "_"))
-      arn  = aws_secretsmanager_secret.app[name].arn
-    }
+  value       = [
+  for name in keys(local.secrets) : {
+    name = upper(replace(name, "-", "_"))
+    arn  = aws_secretsmanager_secret.app[name].arn
+  }
   ]
 
-  sensitive = true
+  sensitive = false
 }
 
 output "kms_key_arn" {
